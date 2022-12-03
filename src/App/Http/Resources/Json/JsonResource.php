5db0b8576a3c97108966804d0383c6ac929ff77f
Norman Huth <norman@huth.it>
2022-12-03 23:59:19 +0100

    Add a simple route to JsonResource

--- src/App/Http/Resources/Json/JsonResource.php
+++ src/App/Http/Resources/Json/JsonResource.php
@@ -2,10 +2,20 @@
 
 namespace NormanHuth\HelpersLaravel\App\Http\Resources\Json;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource as BaseResource;
+use JsonSerializable;
 
 class JsonResource extends BaseResource
 {
+    /**
+     * Optional: Set a key name to add a simple route in the resource array
+     *
+     * @var string|null
+     */
+    protected ?string $addSimpleShowRoute = null;
+
     /**
      * Create a new anonymous resource collection.
      *
@@ -19,5 +29,22 @@ class JsonResource extends BaseResource
                 $collection->preserveKeys = (new static([]))->preserveKeys === true;
             }
         });
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|JsonSerializable|Arrayable
+    {
+        $array = parent::toArray($request);
+
+        if ($this->addSimpleShowRoute) {
+            $array = array_merge($array, [$this->addSimpleShowRoute => $request->getPathInfo().'/'.$this->getRouteKey()]);
+        }
+
+        return $array;
     }
 }
