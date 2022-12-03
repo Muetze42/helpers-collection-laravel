6f3efa926c1c6603c47022e306fff84a7d0c50ea
Norman Huth <norman@huth.it>
2022-12-03 09:54:35 +0100

    Add a trait for `Laravel-activitylog` by Spatie

---
+++ src/Traits/Spatie/LogsActivityTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace NormanHuth\HelpersLaravel\Traits\Spatie;
+
+use Illuminate\Support\Str;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+trait LogsActivityTrait
+{
+    use LogsActivity;
+
+    protected function getDontLogIfAttributesChangedOnly(): array
+    {
+        return $this->hidden;
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll()
+            ->logExcept(array_merge([
+                'created_at',
+                'updated_at',
+            ], $this->hidden))
+            ->dontLogIfAttributesChangedOnly($this->getDontLogIfAttributesChangedOnly())
+            ->logOnlyDirty()
+            ->useLogName($this->getLogName());
+    }
+
+    protected function getLogName(): string
+    {
+        return $this->formatLogName(get_class($this));
+    }
+
+    protected function formatLogName(string $class): string
+    {
+        $className = Str::kebab(class_basename($class));
+
+        return Str::plural(explode('-', $className)[0]);
+    }
+}
