1bdf4532aecb1fb5291738f688aca3c98517835a
Norman Huth <norman.huth@reaze.com>
2022-12-12 17:01:48 +0100

    Refactor Tags Model for Nova field

--- src/Spatie/Tags/Tag.php
+++ src/Spatie/Tags/Tag.php
@@ -6,7 +6,7 @@ use ArrayAccess;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection as DbCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\Tag as Model;
 use Illuminate\Support\Collection;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
