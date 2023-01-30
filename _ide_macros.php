0ed370d5568bddc099ade1725e6ac426ac768540
Norman Huth <norman@huth.it>
2023-01-30 07:51:15 +0100

    Add macros

---
+++ _ide_macros.php
@@ -0,0 +1,34 @@
+<?php /** @noinspection PhpMultipleClassDeclarationsInspection */
+
+namespace Illuminate\Support {
+    class Carbon {
+        public static function responseToDateTimeString(string $datetime): string {
+
+        }
+        public static function addUserTimezone(string $datetime): string {
+
+        }
+    }
+}
+
+namespace Carbon {
+    class Carbon {
+        public static function responseToDateTimeString(string $datetime): string {
+
+        }
+        public static function addUserTimezone(string $datetime): string {
+
+        }
+        public static function toAppDateString(): void {
+
+        }
+    }
+}
+
+namespace Illuminate\Support {
+    class Collection  {
+        public static function addToValueLabel(): array {
+
+        }
+    }
+}
