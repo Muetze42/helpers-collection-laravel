8730b70e1da6f5367421a85ba89a5205abecf3ec
Norman Huth <norman@huth.it>
2023-08-01 15:31:44 +0200

    Add ConsoleMakeCommand

---
+++ config/helpers-collection.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'commands' => [
+        'console-make-command' => true,
+    ],
+];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,9 +4,20 @@ namespace NormanHuth\HelpersLaravel;
 
 use Illuminate\Support\ServiceProvider as Provider;
 use NormanHuth\Helpers\Exception\FileNotFoundException;
+use NormanHuth\Helpers\Str;
 
 class ServiceProvider extends Provider
 {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/helpers-collection.php', 'helpers-collection'
+        );
+    }
+
     /**
      * Bootstrap any package services.
      *
@@ -16,25 +27,58 @@ class ServiceProvider extends Provider
     public function boot(): void
     {
         $this->publishes([
+            __DIR__.'/../config/helpers-collection.php' => config_path('helpers-collection.php'),
+        ], 'config');
+
+        $this->publishes([
             __DIR__.'/../stubs' => base_path('stubs'),
         ], 'stubs');
 
         if ($this->app->runningInConsole()) {
-            $this->commands($this->getCommands());
-            $this->commands($this->getCommands('Development'));
-
-            if (class_exists('Laravel\Nova\Nova')) {
-                $this->commands($this->getCommands('Nova'));
-            }
-
-            if (class_exists('Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider')) {
-                $this->commands($this->getCommands('IdeHelper'));
-            }
+            $this->registerCommands();
+            $this->registerConfiguredCommands();
         }
     }
 
     /**
-     * Get all package commands
+     * Register the package's custom Artisan commands.
+     *
+     * @return void
+     */
+    protected function registerCommands(): void
+    {
+        $this->commands($this->getCommands());
+        $this->commands($this->getCommands('Development'));
+
+        if (class_exists('Laravel\Nova\Nova')) {
+            $this->commands($this->getCommands('Nova'));
+        }
+
+        if (class_exists('Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider')) {
+            $this->commands($this->getCommands('IdeHelper'));
+        }
+    }
+
+    /**
+     * Register the package's configured custom Artisan commands.
+     *
+     * @return void
+     */
+    protected function registerConfiguredCommands(): void
+    {
+        $commands = array_filter(config('helpers-collection.commands', []));
+
+        if (count($commands)) {
+            $commands = array_map(function ($command) {
+                return '\\'.__NAMESPACE__.'\\Commands\\'.Str::studly($command);
+            }, array_keys($commands));
+
+            $this->commands($commands);
+        }
+    }
+
+    /**
+     * Get package's custom Artisan commands.
      *
      * @param string $directory
      * @return array
