7320e39106b282df429a8cb00123a33cbc6bc764
Norman Huth <norman@huth.it>
2023-08-25 22:06:26 +0200

    Refactor RoutesSave command

--- src/App/Console/Commands/Development/RoutesSave.php
+++ src/App/Console/Commands/Development/RoutesSave.php
@@ -3,8 +3,13 @@
 namespace NormanHuth\HelpersLaravel\App\Console\Commands\Development;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Routing\Router;
+use Illuminate\Routing\ViewController;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Routing\Route;
+use NormanHuth\Helpers\Markdown\Table;
+use ReflectionClass;
 
 class RoutesSave extends Command
 {
@@ -20,7 +25,21 @@ class RoutesSave extends Command
      *
      * @var string
      */
-    protected $description = 'Save the `route:list` output to `/storage/development/routes.txt` or `/storage/development/{name}.txt`';
+    protected $description = 'Save the `route:list` output to `/storage/app/routes.md` or `/storage/app/{name}.md`';
+
+    /**
+     * The router instance.
+     *
+     * @var \Illuminate\Routing\Router
+     */
+    protected Router $router;
+
+    public function __construct(Router $router)
+    {
+        parent::__construct();
+
+        $this->router = $router;
+    }
 
     /**
      * Execute the console command.
@@ -30,10 +49,79 @@ class RoutesSave extends Command
         $name = $this->option('name');
         $name = $name ?: 'routes';
 
-        $name = $name.'.txt';
-        Artisan::call('route:list');
-        Storage::disk('local')->put($name, Artisan::output());
+        $name = $name . '.md';
 
-        $this->info(__('The were saved in the file `:file`', ['name' => $name, 'file' => Storage::disk('local')->path($name)]));
+        $contents = '# Route Export ' . now()->toDateTimeString() . "\n\n";
+
+        $table = new Table();
+
+        $table->addCell('Domain');
+        $table->addCell('Name');
+        $table->addCell('Uri');
+        $table->addCell('Method');
+        $table->addCell('Action');
+        $table->addCell('Middleware');
+
+        $routes = collect($this->router->getRoutes())->map(function (Route $route) {
+            $method = implode('\|', $route->methods());
+            $middleware = array_diff($route->middleware(), $route->excludedMiddleware());
+            $action = ltrim($route->getActionName(), '\\');
+            $name = $route->getName();
+
+            return [
+                'domain' => $route->domain(),
+                'name' => $name,
+                'uri' => '/' . ltrim($route->uri(), '/'),
+                'method' => $method == 'GET|HEAD|POST|PUT|PATCH|DELETE|OPTIONS' ? 'ANY' : $method,
+                'action' => $this->formatAction($action, $name),
+                'middleware' => implode(' \| ', $middleware),
+            ];
+        })->sortBy(['domain', 'uri']);
+
+        $table->addRows($routes);
+
+        $contents .= $table->render();
+        $contents = str_replace('>>>', ' » ', $contents);
+
+        Storage::disk('local')->put($name, trim($contents)."\n");
+        $this->info(__('The were saved in the file `:file`', ['file' => Storage::disk('local')->path($name)]));
+    }
+
+    /**
+     * Get the formatted action.
+     * Based on \Illuminate\Foundation\Console\RouteListCommand
+     *
+     * @param string|null $action
+     * @param string|null $name
+     *
+     * @return string|null
+     */
+    protected function formatAction(?string $action, ?string $name): ?string
+    {
+        if ($action === 'Closure' || $action === ViewController::class) {
+            return $name;
+        }
+
+        $name = $name ? $name . '>>>' : null;
+
+        $rootControllerNamespace = $this->laravel[UrlGenerator::class]->getRootControllerNamespace()
+            ?? ($this->laravel->getNamespace() . 'Http\\Controllers');
+
+        if (str_starts_with($action, $rootControllerNamespace)) {
+            return $name . substr($action, mb_strlen($rootControllerNamespace) + 1);
+        }
+
+        $actionClass = explode('@', $action)[0];
+
+        if (
+            class_exists($actionClass) &&
+            str_starts_with((new ReflectionClass($actionClass))->getFilename(), base_path('vendor'))
+        ) {
+            $actionCollection = collect(explode('\\', $action));
+
+            return $name . $actionCollection->take(2)->implode('\\') . '>>>' . $actionCollection->last();
+        }
+
+        return $name . $action;
     }
 }
