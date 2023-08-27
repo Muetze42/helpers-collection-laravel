1a485a36226e634ce20b6f53bfe672a069eeaa78
Norman Huth <norman@huth.it>
2023-08-27 11:54:32 +0200

    Refactor Commands

--- src/App/Console/Commands/Development/SortLangJsonFiles.php
+++ src/App/Console/Commands/Development/SortLangJsonFiles.php
@@ -2,45 +2,10 @@
 
 namespace NormanHuth\HelpersLaravel\App\Console\Commands\Development;
 
-use Illuminate\Console\Command;
-
-class SortLangJsonFiles extends Command
+/**
+ * @deprecated Use \NormanHuth\HelpersLaravel\App\Console\Commands\Development\SortLangJsonFilesCommand
+ */
+class SortLangJsonFiles extends SortLangJsonFilesCommand
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'sort:lang:json';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Sort the app language files in alphabetic order';
-
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        $this->handleFolder(base_path('vendor/norman-huth/hellofresh-database/lang'));
-    }
-
-    public function handleFolder(string $folder)
-    {
-        $folder = trim($folder, '/\\');
-        foreach (glob($folder.'/*') as $filename) {
-            if (is_dir($filename)) {
-                $this->handleFolder($filename);
-                continue;
-            }
-            if (fileGetExtension($filename) == 'json') {
-                $content = json_decode(file_get_contents($filename), true);
-                ksort($content, SORT_STRING | SORT_FLAG_CASE);
-                file_put_contents($filename, json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
-            }
-        }
-    }
+    //
 }
